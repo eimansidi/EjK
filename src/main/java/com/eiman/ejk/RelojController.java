0f4847a8d14a467e4d2e817f9e4f37d064061de4
Eiman Salek <eimanabde04@gmail.com>
2024-11-14 17:34:23 +0100

    Corregidos nombres, comentarios, JavaDoc y README

--- src/main/java/com/eiman/ejk/RelojController.java
+++ src/main/java/com/eiman/ejk/RelojController.java
@@ -15,6 +15,10 @@ public class RelojController {
     private Timer timer;
     private final String imagePath = "/com/eiman/ejk/imagenes/"; // Ajusta esta ruta según la estructura de tu proyecto
 
+    /**
+     * Inicializa el reloj. Establece la hora actual y configura un temporizador
+     * para actualizar la hora cada segundo.
+     */
     public void initialize() {
         // Llamar a updateClock() inmediatamente para establecer la hora actual al inicio
         updateClock();
@@ -29,6 +33,11 @@ public class RelojController {
         }, 1000, 1000); // Espera 1 segundo antes de la primera ejecución, luego actualiza cada segundo
     }
 
+    /**
+     * Actualiza la hora en los ImageView correspondientes a cada dígito del reloj.
+     * Obtiene la hora, minutos y segundos actuales y actualiza las imágenes
+     * de los dígitos correspondientes.
+     */
     private void updateClock() {
         // Obtiene la hora actual
         LocalDateTime now = LocalDateTime.now();
@@ -45,6 +54,12 @@ public class RelojController {
         setDigitImage(seg2, seconds % 10);   // Unidades de segundos
     }
 
+    /**
+     * Establece la imagen correspondiente al dígito en el ImageView proporcionado.
+     *
+     * @param imageView El ImageView al que se asignará la imagen.
+     * @param digit El dígito que se representará.
+     */
     private void setDigitImage(ImageView imageView, int digit) {
         // Define los nombres de los archivos de imagen para cada dígito
         String[] digitNames = {"ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"};
